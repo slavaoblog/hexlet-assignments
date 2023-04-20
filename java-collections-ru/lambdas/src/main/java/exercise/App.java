d59430ed384f0302b870c684795e6c750edccd1e
@hexlet/cli <support@hexlet.io>
2023-04-20 14:32:18 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -1,6 +1,5 @@
 package exercise;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,9 +1,7 @@
 package exercise;
 
-import java.util.Comparator;
 import java.util.Map;
 import java.util.List;
-import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 // BEGIN
