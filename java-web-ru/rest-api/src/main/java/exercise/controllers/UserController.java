197a93038f2247fa260c2911fcb7fac85194b9cd
@hexlet/cli <support@hexlet.io>
2023-07-09 14:28:23 +0300

    submit java-web-ru/rest-api

---
+++ java-web-ru/rest-api/src/main/java/exercise/controllers/UserController.java
@@ -0,0 +1,73 @@
+package exercise.controllers;
+
+import io.javalin.http.Context;
+import io.javalin.apibuilder.CrudHandler;
+import io.ebean.DB;
+import java.util.List;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.lang3.StringUtils;
+
+public class UserController implements CrudHandler {
+
+    public void getAll(Context ctx) {
+        // BEGIN
+        List<User> users = new QUser()
+                .orderBy()
+                .id.asc()
+                .findList();
+
+        String json = DB.json().toJson(users);
+
+        ctx.json(json);
+        // END
+    };
+
+    public void getOne(Context ctx, String id) {
+
+        // BEGIN
+        User user = new QUser()
+                .id.equalTo(Integer.parseInt(id))
+                .findOne();
+        String json = DB.json().toJson(user);
+
+        ctx.json(json);
+        // END
+    };
+
+    public void create(Context ctx) {
+
+        // BEGIN
+        String body = ctx.body();
+
+        User user = DB.json().toBean(User.class, body);
+        user.save();
+
+        ctx.attribute("flash", "Пользователь успешно создан");
+        // END
+    };
+
+    public void update(Context ctx, String id) {
+        // BEGIN
+        String body = ctx.body();
+        User updatedUser = DB.json().toBean(User.class, body);
+        updatedUser.setId(id);
+        updatedUser.update();
+
+        ctx.attribute("flash", "Пользователь успешно обновлен");
+        // END
+    };
+
+    public void delete(Context ctx, String id) {
+        // BEGIN
+        new QUser()
+                .id.equalTo(Integer.parseInt(id))
+                .delete();
+
+        ctx.attribute("flash", "Пользователь успешно удален");
+        // END
+    };
+}
