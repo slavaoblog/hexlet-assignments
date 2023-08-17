f0eed23adc45e29d6f9907eaf53133341a32ebda
@hexlet/cli <support@hexlet.io>
2023-08-17 18:15:47 +0300

    submit java-spring-ru/service-layer_17.08.23_16.59.02

---
+++ java-spring-ru/service-layer_17.08.23_16.59.02/src/main/java/exercise/repository/CityRepository.java
@@ -0,0 +1,17 @@
+package exercise.repository;
+
+import exercise.model.City;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CityRepository extends CrudRepository<City, Long> {
+
+
+    // BEGIN
+    List<City> findByNameStartingWith(String prefix);
+
+    List<City> findAllByOrderByNameAsc();
+    // END
+}
