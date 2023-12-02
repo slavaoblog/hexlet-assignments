5056afb839cd4fa5a1cd4fe46dcf6db08a8b2b6e
@hexlet/cli <support@hexlet.io>
2023-12-02 13:34:06 +0200

    submit java-spring-ru/search

---
+++ java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -0,0 +1,47 @@
+package exercise.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import exercise.dto.ProductParamsDTO;
+import exercise.model.Product;
+
+// BEGIN
+@Component
+public class ProductSpecification {
+    public Specification<Product> build(ProductParamsDTO params) {
+        return withTitleCont(params.getTitleCont())
+                .and(withPriceGt(params.getPriceGt()))
+                .and(withPriceLt(params.getPriceLt()))
+                .and(withRatingGt(params.getRatingGt()))
+                .and(withCategoryId(params.getCategoryId()));
+    }
+
+    private Specification<Product> withTitleCont(String titleCont) {
+        return (root, query, cb) -> {
+            if (titleCont == null) {
+                return cb.conjunction();
+            } else {
+                String loweredTitleCont = titleCont.toLowerCase();
+                return cb.like(cb.lower(root.get("title")), "%" + loweredTitleCont + "%");
+            }
+        };
+    }
+
+    private Specification<Product> withPriceGt(Integer priceGt) {
+        return (root, query, cb) -> priceGt == null ? cb.conjunction() : cb.greaterThan(root.get("price"), priceGt);
+    }
+
+    private Specification<Product> withPriceLt(Integer priceLt) {
+        return (root, query, cb) -> priceLt == null ? cb.conjunction() : cb.lessThan(root.get("price"), priceLt);
+    }
+
+    private Specification<Product> withRatingGt(Double ratingGt) {
+        return (root, query, cb) -> ratingGt == null ? cb.conjunction() : cb.greaterThan(root.get("rating"), ratingGt);
+    }
+
+    private Specification<Product> withCategoryId(Long categoryId) {
+        return (root, query, cb) -> categoryId == null ? cb.conjunction() : cb.equal(root.get("category").get("id"), categoryId);
+    }
+}
+// END
